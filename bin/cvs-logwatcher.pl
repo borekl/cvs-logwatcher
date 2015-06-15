5b4bd8627f49b03b3c5d9351a0374f024936d963
Borek Lupomesky <borek.lupomesky@vodafone.com>
2015-06-15 15:25:28 +0200

    Configured expect chats
    Now the expect-response pairs are configured in the
    JSON configuration file.

--- bin/cvs-logwatcher.pl
+++ bin/cvs-logwatcher.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl
 
 #=============================================================================
-# CVS_CISCO
+# CVS LOG WATCHER
 #
-# Script to pull configuration log out of a Cisco device after detecting
+# Script to pull configuration log out of a network device after detecting
 # change by observing the device's logfile. The usual way of getting the
 # config file is by configuring a transfer by setting SNMP variables and
 # getting the file over TFTP.
@@ -38,7 +38,7 @@ use JSON;
 my ($cfg, $logger);
 my $dev               = 0;
 my $prefix            = '/opt/cvs/%s';
-
+my $id                = '[cvs]';
 
 
 #=============================================================================
@@ -46,7 +46,9 @@ my $prefix            = '/opt/cvs/%s';
 #=============================================================================
 
 #=============================================================================
-# Gets admin group name from hostname.
+# Gets admin group name from hostname. Admin group is decided based on
+# regexes define in "groups" top-level config object. If no match is found,
+# "defgroup" key is tried under "logfiles"->{logid}.
 #=============================================================================
 
 sub get_admin_group
@@ -110,9 +112,9 @@ sub snmp_get_value
   
   #--- run the command
 
-  $logger->debug(qq{[cvs-csc] Cmd: $cmd});  
+  $logger->debug(qq{$id Cmd: $cmd});  
   open(FH, "$cmd |") || do {
-    $logger->fatal(qq{[cvs-csc] Failed to execute SNMP get ($cmd), aborting});
+    $logger->fatal(qq{$id Failed to execute SNMP get ($cmd), aborting});
     die;
   };
   my $val = <FH>;
@@ -138,27 +140,34 @@ sub run_expect_batch
   #--- arguments
   
   my (
-    $spawn,        # 1. spawn command
-    $expect,       # 2. (arrayref) batch of expect-response pairs
-    $sleep         # 3. optional sleep time after every command
+    $expect_def,   # 1. expect conversion definitions from the config.json
+    $host          # 2. hostname
   ) = @_;
-
+    
+  #--- variables
+  
+  my $spawn = $expect_def->{'spawn'};
+  my $sleep = $expect_def->{'sleep'};
+  my $chat = $expect_def->{'chat'};
   
   #--- spawn command
-  
-  $logger->info("[cvs-csc] Spawning Expect instance ($spawn)");
+
+  $spawn =~ s/%h/$host/g;
+  $logger->info("$id Spawning Expect instance ($spawn)");
   my $exh = Expect->spawn($spawn) or do {
-    $logger->fatal("[cvs-csc] Failed to spawn Expect instance ($spawn)");
+    $logger->fatal("$id Failed to spawn Expect instance ($spawn)");
     die;
   };
   $exh->log_stdout(0);
 
   eval {  #<--- eval begins here ---------------------------------------------
 
-    for my $row (@$expect) {
+    for my $row (@$chat) {
+      my $chat_send = $row->[1];
+      $chat_send =~ s/%h/$host/g;
       $logger->debug(
-        "[cvs-csc] Expect command: " . 
-        ($row->[1] eq "\r" ? '[CR]' : $row->[1])
+        "$id Expect command: " . 
+        ($chat_send eq "\r" ? '[CR]' : $chat_send)
       );
       $exh->expect(undef, '-re', $row->[0]) or die;
       $exh->print($row->[1]);
@@ -169,7 +178,7 @@ sub run_expect_batch
 
   sleep($sleep) if $sleep;  
   if($@) {
-    $logger->error('[cvs-csc] Expect failed');
+    $logger->error(qq{$id Expect failed});
     $exh->soft_close();
     die;
   }
@@ -198,36 +207,52 @@ $prefix = sprintf($prefix, $dev ? 'dev' : 'prod');
 {
   local $/;
   my $fh;
-  open($fh, '<', "$prefix/cfg/config.json");
+  open($fh, '<', "$prefix/cfg/config.json") or die;
   my $cfg_json = <$fh>;
   close($fh);
-  $cfg = decode_json($cfg_json);
+  $cfg = decode_json($cfg_json) or die;
 }
 
 #--- initialize Log4perl logging system
 
 Log::Log4perl->init("$prefix/cfg/logging.conf");
-$logger = get_logger('CVS::Cisco');
+$logger = get_logger('CVS::Main');
 
 #--- title
 
-$logger->info('[cvs-csc] --------------------------------------');
-$logger->info('[cvs-csc] NetIT CVS // Cisco Log Watcher started');
-$logger->info('[cvs-csc] Mode is ', $dev ? 'development' : 'production');
+$logger->info(qq{$id --------------------------------------});
+$logger->info(qq{$id NetIT CVS // Cisco Log Watcher started});
+$logger->info(qq{$id Mode is }, $dev ? 'development' : 'production');
+
+#--- processing command-line
+
+my $logdef;
+if($ARGV[0] && exists $cfg->{'logfiles'}{lc($ARGV[0])}) {
+  $logdef = lc($ARGV[0]);
+  $logger->info(qq{$id Log selected: $logdef});
+  $id = "[cvs-$logdef]";
+} else {
+  $logger->fatal(qq{$id Invalid logfile selected});
+  $logger->info(
+    qq{$id Following logfiles defined: }
+    . join(', ', keys %{$cfg->{'logfiles'}})
+  );
+  exit();
+}
 
 #--- opening the logfile
 
 my $logfile = sprintf(
                 '%s/%s',
                 $cfg->{'config'}{'logprefix'},
-                $cfg->{'logfiles'}{'cisco'}{'logfile'}
+                $cfg->{'logfiles'}{$logdef}{'logfile'}
               );
-$logger->info("[cvs-csc] Opening logfile $logfile");
+$logger->info("$id Opening logfile $logfile");
 open(LOG, "tail -f -c 0 $logfile|");
 
 #--- compile matching regex
 
-my $regex_src = $cfg->{'logfiles'}{'cisco'}{'match'};
+my $regex_src = $cfg->{'logfiles'}{$logdef}{'match'};
 my $regex = qr/$regex_src/;
 
 #--- logfile reading loop
@@ -243,87 +268,79 @@ while (<LOG>) {
 
   /$regex/ && do {
 
-    chomp;
-    $logger->debug(qq{[cvs-csc] Line matched: "$_"});
-    my $host = $4;
-    my $message = "$1 $2 $3 $5"; 
+    #------------------------------------------------------------------------
+    #--- cisco --------------------------------------------------------------
+    #------------------------------------------------------------------------
 
-    $logger->info(qq{[cvs-csc] Source host: $host (from syslog)});
-    $logger->info('[cvs-csc] Message: ', $message);
+    if($logdef eq 'cisco') {
 
-    #--- get hostnam via SNMP
-    
-    # FIXME: Why messing with hostname from snmp/logfile?
-    # Shouldn't it suffice to use one or another?
+      chomp;
+      $logger->debug(qq{$id Line matched: "$_"});
+      my $host = $4;
+      my $message = "$1 $2 $3 $5"; 
 
-    $logger->info('[cvs-csc] Getting hostname from SNMP');            
-    my $host2 = snmp_get_value($host, 'cisco', 'hostName');
-    $host2 = $host if !$host2;
-    $host2 =~ s/\..*$//;
-    $logger->info(qq{[cvs-csc] Source host: $host2 (from SNMP)});
-    
-    $logger->info('[cvs-csc] Checking IOS version');	
-    my $sysdescr = snmp_get_value($host, 'cisco', 'sysDescr');
+      $logger->info(qq{$id Source host: $host (from syslog)});
+      $logger->info(qq{$id Message: }, $message);
 
-    #--- assign admin group
-    
-    my $group = get_admin_group($host2, 'cisco');
-    if($group) {
-      $logger->info(qq{[cvs-csc] Admin group '$group'});
-    } else {
-      $logger->error(qq{[cvs-csc] No admin group for $host2, skipping});
-      next;
-    }
-    
-    #--- special handling for IOS XR routers
-          
-    # IOS XR is handled by connecting over SSH
-    # apparently, this is merely done because TFTP doesn't
-    # properly handle mgmt interface in an VRF.
-    # On the IOS XR boxes there's "backup-config" alias defined as:
-    #
-    # alias backup-config copy running-config tftp://172.20.113.120/cs/<file> vrf MGMT
-    #
-    # where file must be host's hostname in lowercase
+      #--- get hostname via SNMP
+      
+      # FIXME: Why messing with hostname from snmp/logfile?
+      # Shouldn't it suffice to use one or another?
 
-    my $ios_type = 'normal';
-    if($sysdescr =~ /Cisco IOS XR/) {
-      $logger->info(qq{[cvs-csc] IOS XR detected on $host2});
-      $ios_type = 'xr';
-      my $ssh_login = $cfg->{'logfiles'}{'cisco'}{'ssh_login'};
-      my $ssh_pass  = $cfg->{'logfiles'}{'cisco'}{'ssh_pass'};
-      my $ssh_cmd   = $cfg->{'logfiles'}{'cisco'}{'ssh_cmd'};
-      my $ssh_run   = join(' ',
-                        ($cfg->{'logfiles'}{'cisco'}{'ssh_run'},
-                        '-l', $ssh_login, $host)
-                      );
-      run_expect_batch(
-        $ssh_run,
-        [
-          [ 'password:',                   "$ssh_pass\r" ],
-          [ '^RP/.*/RSP.*/CPU.*:.*NB00#$', "$ssh_cmd\r" ],
-          [ 'control-c to abort',          "\r" ],
-          [ 'control-c to abort',          "\r" ],
-          [ '^RP/.*/RSP.*/CPU.*:.*NB00#$', "exit\r" ]
-        ],
-        1
+      $logger->info(qq{$id Getting hostname from SNMP});
+      my $host2 = snmp_get_value($host, 'cisco', 'hostName');
+      $host2 = $host if !$host2;
+      $host2 =~ s/\..*$//;
+      $logger->info(qq{$id Source host: $host2 (from SNMP)});
+      
+      $logger->info(qq{$id Checking IOS version});
+      my $sysdescr = snmp_get_value($host, 'cisco', 'sysDescr');
+
+      #--- assign admin group
+      
+      my $group = get_admin_group($host2, 'cisco');
+      if($group) {
+        $logger->info(qq{$id Admin group '$group'});
+      } else {
+        $logger->error(qq{$id No admin group for $host2, skipping});
+        next;
+      }
+      
+      #--- special handling for IOS XR routers
+            
+      # IOS XR is handled by connecting over SSH
+      # apparently, this is merely done because TFTP doesn't
+      # properly handle mgmt interface in an VRF.
+      # On the IOS XR boxes there's "backup-config" alias defined as:
+      #
+      # alias backup-config copy running-config tftp://172.20.113.120/cs/<file> vrf MGMT
+      #
+      # where file must be host's hostname in lowercase
+
+      my $ios_type = 'normal';
+      if($sysdescr =~ /Cisco IOS XR/) {
+        $logger->info(qq{$id IOS XR detected on $host2});
+        $ios_type = 'xr';
+        run_expect_batch(
+          $cfg->{'logfiles'}{$logdef}{'expect'},
+          $host
+        );
+        
+      }
+
+      #--- run RCS commit
+      
+      my $exec = sprintf(
+        '%s/bin/cvs_new_version.sh %s %s "%s" %s %s %s',
+        $prefix, $host, $host2, $message, 'cisco', $group, $ios_type
       );
+      
+      $logger->info(qq{$id Running CVS script});
+      $logger->debug(qq{$id Running command '$exec'});
+      system($exec);
+      $logger->info(qq{$id CVS script finished});
     }
 
-    #--- run RCS commit
-    
-    my $exec = sprintf(
-      '%s/bin/cvs_new_version.sh %s %s "%s" %s %s %s',
-      $prefix, $host, $host2, $message, 'cisco', $group, $ios_type
-    );
-    
-    $logger->info(qq{[cvs-csc] Running CVS script});
-    $logger->debug(qq{[cvs-csc] Running command '$exec'});
-    system($exec);
-    $logger->info(qq{[cvs-csc] CVS script finished});
   }
     
 }
-
-
-
